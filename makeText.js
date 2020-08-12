60e60cbd9efdaec129f872bd81d5325658e3b036
Rain Babauta <rainbabauta1@gmail.com>
2020-08-11 17:06:52 -0700

    finished result before code review

--- makeText.js
+++ makeText.js
@@ -1,2 +1,27 @@
+"use strict";
 /** Command-line tool to generate Markov text. */
+const fsP = require("fs/promises");
+const { MarkovMachine } = require("./markov");
 
+async function makeText(path) {
+  let content;
+  try {
+    content = await fsP.readFile(path, "utf8");
+  } catch(err) {
+    console.error(`Error reading ${path}: ${err} in makeText.`);
+    process.exit(1);
+  }
+
+  return content;
+}
+
+async function demoChain() {
+  let path = process.argv[3];
+  let content = await makeText(path);
+
+  let mark = new MarkovMachine(content);
+  mark.makeChains()
+  mark.getText()
+}
+
+demoChain()
--- markov.js
+++ markov.js
@@ -1,3 +1,4 @@
+"use strict";
 /** Textual markov chain generator */
 
 
@@ -6,8 +7,10 @@ class MarkovMachine {
   /** build markov machine; read in text.*/
 
   constructor(text) {
-    let words = text.split(/[ \r\n]+/);
-    // MORE CODE HERE
+    console.log("constructor ran, this is text:", text);
+    this.words = text.split(/[ \r\n]+/);
+    this.chains = {};
+    this.text = "";
   }
 
   /** set markov chains:
@@ -16,13 +19,77 @@ class MarkovMachine {
    *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
 
   makeChains() {
-    // MORE CODE HERE
+    console.log("makeChains ran");
+
+    for (let i=0; i<this.words.length; i++) {
+      let currentWord = this.words[i];
+      let nextWord = this.words[i+1]
+      console.log("currentWord is:", currentWord, "and nextWord is:", nextWord, "this is i type:", typeof i);
+
+      // if word exists in the chains obj:
+      if (this.chains[currentWord]) {
+        console.log("word exists");
+
+        // check if there is a next word
+        if (nextWord === undefined) {
+          // if there is no word, put null in for currentWord
+          this.chains[currentWord].push(null);
+        } else {
+          this.chains[currentWord].push(nextWord);
+        }
+      } else {
+        console.log("word doesnt exist");
+        // create an empty array
+        this.chains[currentWord] = [];
+
+        // check if there is a next word
+        if (nextWord === undefined) {
+          // if there is no word, put null in for currentWord
+          this.chains[currentWord].push(null);
+        } else {
+          this.chains[currentWord].push(nextWord);
+        }
+      }
+    } 
+    console.log("this is chains:", this.chains);
   }
 
 
   /** return random text from chains */
 
   getText(numWords = 100) {
-    // MORE CODE HERE
+    console.log("getText ran");
+    // 1. randomly select a word from this.words
+    // 2. append it to this.text
+    // 3. find the selected word in this.chains
+    // 4. randomly select a word from this.chains[word]
+    // 5. repeat steps 2-4
+
+    let randomIdx = Math.floor(Math.random() * this.words.length)
+    let randomWord = this.words[randomIdx];
+
+    let nextWord = randomWord;
+
+    let count = 0;
+    while (nextWord !== null && count <= numWords) {
+      console.log("inside while loop, nextWord is:", nextWord, nextWord === null);
+      this.text += nextWord + " ";
+      nextWord = this.getNextWord(nextWord)
+      count++;
+    }
+
+    console.log("this is the result:", this.text);
+  }
+
+  // given a word from this.words, find currentWord in this.chains
+  // and return a random word from this.chains[word]
+  getNextWord(currentWord) {
+    console.log("getNextWord ran and currentWord is:", currentWord);
+    let randomIdx = Math.floor(Math.random() * this.chains[currentWord].length);
+    let newWord = this.chains[currentWord][randomIdx];
+
+    return newWord;
   }
 }
+
+module.exports = { MarkovMachine };
